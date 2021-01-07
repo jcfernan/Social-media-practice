d76c867ea9b7bb542c96dd8aa7cc6b4aa6009286
Carlos Fernandez <jcfernan@live.com>
2021-01-07 16:19:12 -0700

    Updated key

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,7 @@ class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :username
   # has_many :posts
+  # set_key_transform :camel_lower
   attribute :post_serialized do |user|
     PostSerializer.new(user.posts)
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Post.destroy_all
+
+user1 = User.create(username: "Carlos", password: "password")
+user2 = User.create(username: "Tim", password: "password")
+user3 = User.create(username: "Kelsey", password: "password")
+user4 = User.create(username: "Matt", password: "password")
+
+post1= Post.create(content: "Hi all", user_id: 32)
+post2= Post.create(content: "Hi there", user_id: 33)
+post3= Post.create(content: "Hi again", user_id: 34)
+post4= Post.create(content: "Hi Ho", user_id: 35)
